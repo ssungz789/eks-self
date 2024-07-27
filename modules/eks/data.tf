3130211246724be2925cae0f33cf8a23dacedd3c
ssungz789 <nin7344@naver.com>
2024-07-27 19:32:04 +0900

    data.tf collection

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -2,6 +2,10 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
